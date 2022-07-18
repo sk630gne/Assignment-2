89c3275ff1c28a8b2f87341e9ff9696dfb14d7fa
suraj <sk9149758@gmail.com>
2022-07-18 23:05:27 +0530

    the second commit

---
+++ q4.py
@@ -0,0 +1,12 @@
+#Armstrong Number Program In Python
+number = int(input("enter the number"))
+temp = number
+add_sum = 0
+while temp!=0:
+    k = temp%10
+    add_sum +=k*k*k
+    temp = temp//10
+if add_sum==number:
+    print('Armstrong Number')
+else:
+    print('Not a Armstrong Number')
